b3258ec62d84912ae931e4f85924548bc97f78fc
fukiame <fukiame@proton.me>
2023-08-06 16:04:59 +0700

    yup, completely nuek python2 y not

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,15 +30,14 @@ repo_name="${GITHUB_REPOSITORY/*\/}"
 zipper_path="${ZIPPER_PATH:-zipper}"
 kernel_path="${KERNEL_PATH:-.}"
 name="${NAME:-$repo_name}"
-python_version="${PYTHON_VERSION:-3}"
 
 msg "Updating container..."
 apt update && apt upgrade -y
 msg "Installing essential packages..."
 apt install -y --no-install-recommends git make bc bison openssl \
     curl zip kmod cpio flex libelf-dev libssl-dev libtfm-dev wget \
-    device-tree-compiler ca-certificates python3 python2 xz-utils
-ln -sf "/usr/bin/python${python_version}" /usr/bin/python
+    device-tree-compiler ca-certificates python3 xz-utils
+ln -sf "/usr/bin/python3" /usr/bin/python
 set_output hash "$(cd "$kernel_path" && git rev-parse HEAD || exit 127)"
 msg "Installing toolchain..."
 if [[ $arch = "arm64" ]]; then
